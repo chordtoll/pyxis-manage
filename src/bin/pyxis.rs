2af19a4643256b3593d3ebd720554939e4ec42fd
chordtoll <git@chordtoll.com>
2022-01-21 20:41:27 -0800

    Change clap setup away from deprecated method

--- src/bin/pyxis.rs
+++ src/bin/pyxis.rs
@@ -1,26 +1,29 @@
-const VERSION: &str = env!("CARGO_PKG_VERSION");
-
+use clap::{crate_version, App, Arg};
 use pyxis_manage::*;
 
-#[macro_use]
-extern crate clap;
-
 fn main() {
-    let matches = clap_app!(pyxis =>
-        (version: VERSION)
-        (about: "The pyxis package manager. Quickly builds and deploys images for netboot.")
-        (@subcommand parcel =>
-            (@subcommand build =>
-                (@arg INPUT: +required "The package to build. provider|package.")
-            )
+    let matches = App::new("pyxis")
+        .version(crate_version!())
+        .author("chordtoll")
+        .subcommand(
+            App::new("parcel").subcommand(
+                App::new("build").arg(
+                    Arg::new("INPUT")
+                        .required(true)
+                        .help("The package to build. provider|package."),
+                ),
+            ),
         )
-        (@subcommand image =>
-            (@subcommand build =>
-                (@arg MANIFEST: +required "The package manifest from which to build the image")
-            )
+        .subcommand(
+            App::new("image").subcommand(
+                App::new("build").arg(
+                    Arg::new("MANIFEST")
+                        .required(true)
+                        .help("The package manifest from which to build the image"),
+                ),
+            ),
         )
-    )
-    .get_matches();
+        .get_matches();
     if let Some(matches) = matches.subcommand_matches("parcel") {
         if let Some(matches) = matches.subcommand_matches("build") {
             pyxis_parcel_build_named(matches.value_of("INPUT").unwrap())
